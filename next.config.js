99bfb19c50517bd6a4e046c852ebeaef7e34aa21
Andrej Filipov <af1rumbusiness05@gmail.com>
2025-03-28 15:09:32 +0100

    chore: remove i18n config for App Router compatibility

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    i18n: {
-      locales: ['sr', 'en'],
-      defaultLocale: 'sr',
-      localeDetection: true,
-    },
+    
   };
   
   module.exports = nextConfig;
